1dd78adbe412717bdcf75ec7dd1d39cef9e3e094
hwu5542 <45774204+hwu5542@users.noreply.github.com>
2020-04-27 20:29:24 -0500

    Update Catalog.sql

--- Catalog.sql
+++ Catalog.sql
@@ -2,11 +2,9 @@ DROP TABLE IF EXISTS Product;
 DROP TABLE IF EXISTS Order;
 
 CREATE TABLE Product(ProductNum int not null auto_increment, number int not null, ProductLoc varchar(50) not null, ProductAmount int not null,
-  PRIMARY KEY (ProductNum),
-  FOREIGN KEY (number) REFERENCES parts(number) ON DELETE CASCADE);
+  PRIMARY KEY (ProductNum));
 CREATE TABLE Order(OrderID int not null auto_increment, WeightBrackets varchar(50) not null, ShipCharge float not null,
   CusName varchar(50) not null, CusEmail varchar(50) not null, CusMail varchar(50), Date varchar(50), ShipStatus varchar(50),
   ProductNum int not null, number int not null, OrderAmount int not null,
   PRIMARY KEY (OrderID),
-  FOREIGN KEY (ProductNum) REFERENCES Product(ProductNum) ON DELETE CASCADE,
-  FOREIGN KEY (number) REFERENCES parts(number) ON DELETE CASCADE);
+  FOREIGN KEY (ProductNum) REFERENCES Product(ProductNum) ON DELETE CASCADE);
