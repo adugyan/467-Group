fb617e01a1b96e27b07355182b6c6838f7a566c4
hwu5542 <45774204+hwu5542@users.noreply.github.com>
2020-04-28 19:48:13 -0500

    Update Catalog.sql

--- Catalog.sql
+++ Catalog.sql
@@ -11,5 +11,5 @@ CREATE TABLE Orders(OrderID int not null auto_increment, WeightBrackets varchar(
 INSERT INTO Product VALUES (11, 12, 'A-1-1', 13);
 INSERT INTO Product VALUES (21, 22, 'B-1-1', 23);
 
-INSERT INTO Orders VALUES (101, 'Heavy', 102.33, 'Aaron', 'aaron@mail.com', '103 W Apache St. Arlington Heights IL.', '04/27/2020', 'New Order', 104, 105);              
-INSERT INTO Orders VALUES (201, 'Light', 202.33, 'Brian', 'Brian@mail.com', '203 E Bussie Rd. Bensenville IL.', '04/26/2020', 'Shipped', 204, 205);              
+INSERT INTO Orders VALUES (101, 'Heavy', 102.33, 'Aaron', 'aaron@mail.com', '103 W Apache St. Arlington Heights IL.', '04/27/2020', 'New Order', 12, 105);              
+INSERT INTO Orders VALUES (201, 'Light', 202.33, 'Brian', 'Brian@mail.com', '203 E Bussie Rd. Bensenville IL.', '04/26/2020', 'Shipped', 22, 205);              
